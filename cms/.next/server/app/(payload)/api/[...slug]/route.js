9bf0c0b1febc925615918b9938146da3f6b6603f
Jozoxm <m.youssef.guide@gmail.com>
2026-05-14 15:05:59 +0300

    Fix submodule configuration

---
+++ cms/.next/server/app/(payload)/api/[...slug]/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/(payload)/api/[...slug]/route.js")
+R.c("server/chunks/[root-of-the-server]__0k4_m-~._.js")
+R.c("server/chunks/[root-of-the-server]__0.v1c17._.js")
+R.c("server/chunks/node_modules_0x146mc._.js")
+R.c("server/chunks/_next-internal_server_app_(payload)_api_[___slug]_route_actions_0fi7ygn.js")
+R.m(182694)
+module.exports=R.m(182694).exports
